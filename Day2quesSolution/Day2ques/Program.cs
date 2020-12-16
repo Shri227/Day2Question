83cd5153b782a6dd6e3b55aea3b7f8f024fe974a
SHRISHTI SONI <SHRISHTI.SONI@lntinfotech.com>
2020-12-16 16:47:02 +0530

    change2

--- Day2quesSolution/Day2ques/Program.cs
+++ Day2quesSolution/Day2ques/Program.cs
@@ -10,6 +10,35 @@ namespace Day2ques
     {
         static void Main(string[] args)
         {
+
+            int a, b, i, j, flag;
+  
+            Console.WriteLine("Enter lower bound of the range: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nEnter upper bound of the range: ");
+            b = int.Parse(Console.ReadLine());
+
+            for (i = a; i <= b; i++)
+            {
+                if (i == 1 || i == 0)
+                    continue;
+                flag = 1;
+
+                for (j = 2; j <= i / 2; ++j)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = 0;
+                        break;
+                    }
+                }
+
+                if (flag == 1)
+                    Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
         }
     }
 }
